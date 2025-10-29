38985b63bf4c8964af88db0fa3441a75ff715ece
racenis <martinsrasaolaslasa@gmail.com>
2025-10-29 21:28:38 +0200

    Added listbox example

---
+++ examples/02_textbox_list/example.cpp
@@ -0,0 +1,109 @@
+#include "../../src/frontend/cpp/perse.h"
+#include "../../src/frontend/cpp/hooks.h"
+
+using namespace perse;
+
+#include <iostream>
+
+Widget Root() {
+	SetContext("Root");
+	
+	auto list_box = ListBox({
+		.width = 128,
+		.height = 96
+	});
+	
+	auto [texts, set_texts] = UseState<std::vector<std::string>>({
+		"item #1",
+		"item #2",
+		"item #4",
+	});
+	auto [selected, set_selected] = UseState(-1);
+	auto [new_text, set_new_text] = UseState(std::string("item #7"));
+	
+	std::vector<Widget> items;
+	
+	for (size_t i = 0; i < texts.size(); i++) {
+		items.push_back({Item({
+			.title = texts[i],
+			.onclick = [=](){
+				set_selected(i);
+			}
+		})});
+	}
+
+	list_box << items;
+	
+	return Window({
+		.width = 240,
+		.height = 240,
+		.x = -1,
+		.y = -1,
+		.title = "List Manager v1.0"
+	}) << Inside({
+		VerticalLayout({}) << Inside({
+			AbsoluteLayout({}),
+			TextField({
+				.width = 128,
+				.height = 24,
+				.text = new_text,
+				.onchange = [=](std::string new_value){
+					set_new_text(new_value);
+				},
+				.onsubmit = [=]() mutable {
+					texts.push_back(new_text);
+					set_texts(texts);
+					
+					set_new_text("");
+				}
+			}),
+			AbsoluteLayout({.height=8}),
+			HorizontalLayout({
+				.width = 128,
+				.height = 24, //commenting this out breaks everything? TODO:fix
+			}) << Inside({
+				Button({
+					.height = 24,
+					.text = "Add",
+					.onclick = [=]() mutable {
+						texts.push_back(new_text);
+						set_texts(texts);
+						
+						set_new_text("");
+					}
+				}),
+				Button({
+					.height = 24,
+					.text = "Remove",
+					.onclick = [=](){
+						if (selected < 0) {
+							std::cout << "cannot!!!" << std::endl;
+							return;
+						}
+						
+						auto text_cpy = texts;
+						text_cpy.erase(text_cpy.begin() + selected);
+						set_texts(text_cpy);
+						
+						set_selected(-1);
+					}
+				}),
+			}),
+			AbsoluteLayout({.height=8}),
+			list_box,
+			AbsoluteLayout({})
+		})
+	});
+}
+
+int main(int argc, const char** argv) {
+	Init();
+
+	SetRoot(Root);
+
+	while (Wait()) {
+		
+	}
+	
+	return 0;
+}
